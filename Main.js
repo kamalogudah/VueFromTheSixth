e3442f2630a25cda5b45c8be932c71446a994231
kamalogudah <mcpaul2058@gmail.com>
2018-05-06 15:29:15 +0300

    Binding to Radio BUttons

--- Main.js
+++ Main.js
@@ -4,7 +4,8 @@ var growler = new Vue({
     appName: 'Growler',
     query: '',
     isPowerSyntaxEnabled: false,
-    searchIndexes: []
+    searchIndexes: [],
+    otherSearchIndexes: []
 
   }
 })
